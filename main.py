a830df6052767244ab40bdd6557a1837004f4799
Chris Rice <chrisrice93@gmail.com>
2015-07-08 11:36:35 +0200

    cool cool cli cool

--- main.py
+++ main.py
@@ -1,18 +1,13 @@
 from function_holder import FunctionHolder
+from cli import CLI
 
 def testFunction():
     print("hello, world. testing, testing...")
     return 22
 
-function_holder_test = FunctionHolder()
-
+function_holder = FunctionHolder()
 # Add a simple function to the function_holder
-function_holder_test.addFunction("test", testFunction, 0)
-while not function_holder_test.isEnabled():
-    # Take the users input as a command and send it to the FunctionHolder instance
-    users_input = input('>? ')
-    result = function_holder_test.executeCommand(users_input)
+function_holder.addFunction("test", testFunction, 0)
 
-    # If something was returned, print it
-    if result:
-        print(result)
+cli = CLI(function_holder)
+cli.run()
